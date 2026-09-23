62fdf3d6e4dab5d0aa62de49a2f0498fe210e9cb
Godwin Robby <godwinrobby@gmail.com>
2026-09-23 14:51:51 +0530

    Clean up patch scripts

--- patch_homepage.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/website/HomePage.tsx', 'utf8');
-
-content = content.replace(
-  "onClick={() => onNavigate('loans')}",
-  "onClick={() => onNavigate('home-loans')}"
-);
-
-fs.writeFileSync('src/components/website/HomePage.tsx', content);
--- patch_navbar.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/common/Navbar.tsx', 'utf8');
-
-content = content.replace(
-  "{ id: 'loans', label: 'Home Loan', icon: Home },",
-  "{ id: 'home-loans', label: 'Home Loans', icon: Home },"
-);
-
-fs.writeFileSync('src/components/common/Navbar.tsx', content);
